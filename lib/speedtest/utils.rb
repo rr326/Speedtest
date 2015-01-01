090888b5831e1d38a3ea21a3dcee4885e0918bdd
Ross Rosen <rrosen326@gmail.com>
2015-01-01 15:50:46 -0800

    refactored create_file - now MUCH simpler

--- lib/speedtest/utils.rb
+++ lib/speedtest/utils.rb
@@ -2,46 +2,36 @@ require 'aws-sdk'
 module Speedtest
   class Utils
     def initialize
-      @file_tmpl = "bytes_%s%s.txt"
+      @file_tmpl = 'bytes_%s%s.txt'
       @bucket = 'rrosen326.speedtest'
+      @units = {
+        :ONE => 1,
+        :KB => 1_000,
+        :MB => 1_000_000
+      }
     end
 
-    # Create a file of size bytes (units in ['KB', 'MB', nil (1)])
+    # Create a file of size bytes (units in [:KB, :MB, nil/:ONE (1)])
     #
     # Note - this uses decimal vales for KB/MB.  So 1 MB will be 10^6 bytes, but
     #  will show up as 977K on the file system. (See http://en.wikipedia.org/wiki/Megabyte)
-    def create_file(size, path, overwrite=false, units=nil)
-      raise 'Improper value for units. Must be MB, KB, or nil(1) ' if units and not %w(KB MB).include? units.to_s.upcase
+    def nbyte_string(size, units=:ONE)
+      units ||= :ONE
 
-      if not overwrite and  File.exists?(path)
-        raise "File (#{path}) already exists and overwrite flag set to false."
-      end
+      raise "Improper units.  Must be in: #{@units.keys}.  Got: #{units}" if not @units.key? units
 
-      case units.to_s.upcase
-        when 'KB'
-          size *= 1_000
-        when 'MB'
-          size *= 1_000_000
-      end
-
-      File.open(path, 'w', 0644) do |f|
-        (1..size).each do |i|
-          f.print "#{i % 10}"
-        end
-      end
-      return path
+      return '0' * size * @units[units]
     end
 
     def aws_create_files
       s3_rw_creds = Aws::Credentials.new(*(ENV['aws_s3_rw'].split(',')))
       s3 = Aws::S3::Client.new(region: 'us-west-2', credentials: s3_rw_creds )
 
-      [[100,nil], [1,'KB'], [1, 'MB']].each do |size, units|
+      [[100,:ONE], [1, :KB], [1, :MB]].each do |size, units|
 
         puts("Writing to aws: size: #{size}  units: #{units}")
         fname = sprintf @file_tmpl, size, units
-        create_file(size, fname, overwrite=true,units=units)
-        body=File.open(fname) {|f| f.read}
+        body = nbyte_string(size, units=units)
         s3.put_object(bucket: @bucket, key: fname, body: body )
       end
     end
--- test/test_speedtest.rb
+++ test/test_speedtest.rb
@@ -4,45 +4,23 @@ require 'fileutils'
 require_relative '../lib/speedtest/utils'
 
 class TestUtils < MiniTest::Unit::TestCase
-  def setup
-    @dir = Dir.mktmpdir
-  end
-
-  def test_create_file
-    file = "#{@dir}/test1.txt"
-    puts file
+  def test_nbyte_string
     st=Speedtest::Utils.new
-    ftext=''
 
+    string = st.nbyte_string(100)
+    assert_equal string.length, 100
 
-    File.open(file, 'w') do |f|
-      assert_raises(RuntimeError) { st.create_file(100, file, false) }
-    end
+    string = st.nbyte_string(1, units=:KB)
+    assert_equal string.length, 1_000
 
-    st.create_file(100, file, true)
-    File.open(file) {|f| ftext=f.read}
-    puts "ftext[0...30]\n#{ftext[0...30]}"
-    assert_equal ftext.length, 100
+    string = st.nbyte_string(1, units=:MB)
+    assert_equal string.length, 1_000_000
 
-    assert_raises(RuntimeError) { st.create_file(100, file, true, units='Foo') }
+    assert_raises(RuntimeError) { st.nbyte_string(100, units=:FOO) }
+    assert_raises(RuntimeError) { st.nbyte_string(100, units='MB') }
 
-    st.create_file(1, file, true, units='kb')
-    File.open(file) {|f| ftext=f.read}
-    assert_equal ftext.length, 1_000
-
-    st.create_file(1, file, true, units='Mb')
-    File.open(file) {|f| ftext=f.read}
-    assert_equal ftext.length, 1_000_000
-
-    # st.create_file(100, 'bytes100.txt', true)
-    # st.create_file(1, 'bytes100000.txt', true, units='KB')
-    # st.create_file(1, 'bytes100000000.txt', true, units='MB')
-    puts "aws_rw_un: #{ENV['aws_s3_rw_un']}"
   end
-
-  def teardown
-    FileUtils.remove_entry(@dir, force = true)
-  end
+  
 end
 
 class TestUtils < MiniTest::Unit::TestCase
